47cbff5a6043cc5fa7c3ab1500ae5d51843327a4
Aaron Gember-Jacobson <agemberjacobson@colgate.edu>
2020-01-08 16:08:16 -0500

    Update script for regrouping APs

--- airwave/run_schedule.py
+++ airwave/run_schedule.py
@@ -5,20 +5,27 @@ Run a schedule for turning APs in a building on and off
 """
 
 from argparse import ArgumentParser
+import logging
 import time
 import paramiko
 
 def handle_event(event, auth):
+    logging.info("Handling event: %s" % (event))
     client = paramiko.client.SSHClient()
     client.load_system_host_keys()
     client.connect(hostname=auth['hostname'], username=auth['username'], 
             password=auth['password'])
-    for ap in event['on']:
-        stdin, stdout, stderr = client.exec_command('echo ON %s' % ap)
-        print(stdout.read())
-    for ap in event['off']:
-        stdin, stdout, stderr = client.exec_command('echo OFF %s' % ap)
-        print(stdout.read())
+    commands = []
+    for group,aps in event.items():
+        for ap in aps:
+            commands.append('ap-regroup ap-name %s %s' % (ap, group))
+
+    for command in commands:
+        logging.info("Executing: %s" % command)
+        stdin, stdout, stderr = client.exec_command(command)
+        logging.info(stdout.read())
+        logging.info(stderr.read())
+
     client.close()
 
 def main():
@@ -30,11 +37,17 @@ def main():
             help='Gap between events')
     arg_parser.add_argument('-a', '--authentication', action='store', 
             required=True, help='File with authentication data')
+    arg_parser.add_argument('-v', '--verbose', action='store_true', 
+            help='Verbose output')
     settings = arg_parser.parse_args()
+
+    logging.basicConfig(format='%(asctime)s %(message)s', 
+            level=(logging.INFO if settings.verbose else logging.WARNING))
+    logging.info(settings)
 
     with open(settings.authentication, 'r') as auth_file:
         auth = eval(auth_file.read())
-    print(auth)
+    logging.info("Loaded authentication data")
 
     gap = settings.gap
     if (gap[-1] == 's'):
